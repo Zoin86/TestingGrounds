10ac6c5c99172f252bf76c59b1db4bc64b5c67c7
Simon Sejer Nielsen <sinie45@gmail.com>
2018-11-08 15:24:46 +0100

    Made Tasks work in C++

---
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,12 @@
+// Simon Sejer Nielsen 2018
+
+#include "ChooseNextWaypoint.h"
+
+
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8* NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Yay for C++ AI!"))
+	return EBTNodeResult::Succeeded;
+}
---
+++ Source/TestingGrounds/ChooseNextWaypoint.h
@@ -0,0 +1,19 @@
+// Simon Sejer Nielsen 2018
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWaypoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent &OwnerComp, uint8* NodeMemory) override;
+};
