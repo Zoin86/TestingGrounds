b50effd64e1a17c968688c962da4a78162126cf2
Simon Sejer Nielsen <sinie45@gmail.com>
2018-11-16 23:40:24 +0100

    Re-removed folders from thirs person and first person folders, as well as animation starter pack Added OnFire event to the gun Made projectile smaller

--- Source/TestingGrounds/Gun.cpp
+++ Source/TestingGrounds/Gun.cpp
@@ -23,7 +23,7 @@ AGun::AGun()
 
 	FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
 	FP_MuzzleLocation->SetupAttachment(FP_Gun);
-	FP_MuzzleLocation->SetRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
+	//FP_MuzzleLocation->SetRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
 
 	// Default offset from the character location for projectiles to spawn
 	GunOffset = FVector(100.0f, 0.0f, 10.0f);
@@ -81,4 +81,5 @@ void AGun::OnFire()
 		}
 	}
 }
+
 
